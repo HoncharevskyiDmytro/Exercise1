bf32a72a37ef2df8505289f415452ca7ff4b99f2
Honcharevskyi Dmytro <dimagonchar7@gmail.com>
2017-11-08 10:40:15 +0100

    HelloUser is added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		while( ! sc.hasNext()) {
+            sc.next();
+        }
+		String nameInputed = sc.next();
+		sc.close();
+		HelloUser userName = new HelloUser(nameInputed);
+		userName.greetUser();
 	}
 
 }
